65cdf0f5034f4106319247b23ac0a072ee039015
bc078 <52862281+bc078@users.noreply.github.com>
2019-08-02 22:44:48 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,25 +1,18 @@
+const Discord = require("discord.js"); // بكج
+const client = new Discord.Client({disableEveryone: true});
+
 client.on('message', message => {
-     if (message.content === ".servers") {
-     let embed = new Discord.RichEmbed()
-  .setColor("#0000FF")
-  .addField("**Server: **" , client.guilds.size)
-  message.channel.sendEmbed(embed);
-    }
-if (message.content === '*help') {
-         let embed = new Discord.RichEmbed()
-.setThumbnail(message.author.avatarURL)    
-      .addField("**:globe_with_meridians: الأوامر العامه**","** **")
-      .addField("***ping :stopwatch:**","**لـ سرعة إتصالك**")
-      .addField("***id :chart_with_downwards_trend:**","**عرض سرعه اتصال البوت**")
-      .addField("***avatar :camping:**","**لـ صور الشخص المختار**")
-      .addField("***roll :1234:**","**لـ القرعه من 1 - 100**")
-      .addField("***server :recycle:**","**لـ معلومات السيرفر**")
-      .addField("***roles :medal: **","**مسح محادثات الشات**")
-      .addField("***say :arrows_counterclockwise:**","**لـ يكرر الكلام اللى تقوله**")
-      .addField("***time :alarm_clock:**","**لـ معرفة الساعة**")
-      .addField("***date **","**لـ معرفة التاريخ**")
-.setColor('RANDOM')
-  message.author.sendEmbed(embed);
-    }
-});
-client.login(process.env.BOT_TOKEN);
+  var prefix = "!"
+    var args = message.content.split(' ').slice(1);
+  var argresult = args.join(' ');
+if (message.content.startsWith(prefix + 's')) {
+                                       message.delete()
+  if (message.author.id !== '358771712839385089') return;
+client.user.setGame(argresult, "http://twitch.tv/y04zgamer");
+    message.channel.sendMessage(`**\`${argresult}\` :تم تغيير الحالة الى**`).then(msg => {msg.delete(7000)});
+
+    
+} 
+
+
+ });
